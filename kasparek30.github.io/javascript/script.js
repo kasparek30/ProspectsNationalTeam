9d27ea0b785953521aec51b53ed14831c3cace04
Kenn Kasparek <kkasparek@kkasparek-mac.local>
2017-01-05 10:07:11 -0600

    PNT

---
+++ kasparek30.github.io/javascript/script.js
@@ -0,0 +1,62 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+function emailSignUp() {
+  var text = document.getElementById('email').value;
+  alert('The following email has been added to our newsletter: ' + text);
+}
+
+function bapForm() {
+  alert('Please verify that the following information is correct before submitting!');
+}
+
+var amountScrolled = 250;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a.back-to-top").on('click', function(event) {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800);
+  });
+});
